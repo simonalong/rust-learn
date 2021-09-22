3732f3f386ee1d7f62fe3cc33e9684b4149a8c42
shizi <zhouzhenyong@shizi-2.local>
2021-09-22 21:31:00 +0800

    add 添加变量的说明

--- greeting/src/main.rs
+++ greeting/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     test_print1();
     test_print2();
     test_print3();
+    test_print4();
 }
 
 fn test_print() {
@@ -23,4 +24,13 @@ fn test_print2() {
 // 输出{}这个字符
 fn test_print3() {
     println!("haode {{}}");
+}
+
+// 测试变量和常量，也就是说Rust里面默认都是常量
+fn test_print4() {
+    let mut b: i32 = 12;
+    println!("test_print4-1 {}", b);
+    b = 32;
+
+    println!("test_print4-2 {}", b);
 }
