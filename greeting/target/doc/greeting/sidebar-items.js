351b765cd64350deca50d5b25d36875ffdf61ba0
shizi <lan.zhu.123@163.com>
2021-09-23 22:51:14 +0800

    add 添加ignore

---
+++ greeting/target/doc/greeting/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["main","测试let a = 12;"],["test_array",""],["test_bool",""],["test_char",""],["test_float",""],["test_int",""],["test_str",""],["test_tup",""],["test_tup2",""]]});
