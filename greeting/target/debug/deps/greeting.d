3b2a456c4fa1aa567d0574c0cb034a9e1e2b233b
shizi <lan.zhu.123@163.com>
2021-09-23 22:52:14 +0800

    delete 删除跟踪

--- greeting/target/debug/deps/greeting.d
+++
@@ -1,5 +0,0 @@
-/Users/zhouzhenyong/project/rust-github/rust-learn/greeting/target/debug/deps/greeting: src/main.rs
-
-/Users/zhouzhenyong/project/rust-github/rust-learn/greeting/target/debug/deps/greeting.d: src/main.rs
-
-src/main.rs:
--- greeting/target/doc/greeting/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["main","测试let a = 12;"],["test_array",""],["test_bool",""],["test_char",""],["test_float",""],["test_int",""],["test_str",""],["test_tup",""],["test_tup2",""]]});
