2dd7a7c8fc65f09e50f77b231dc9ad1c7891d26a
shizi <lan.zhu.123@163.com>
2021-09-23 22:46:31 +0800

    delete 删除target的跟踪

--- greeting/target/debug/deps/greeting-8551893e1171a3d6.d
+++
@@ -1,5 +0,0 @@
-/Users/zhouzhenyong/project/rust-github/rust-learn/greeting/target/debug/deps/greeting-8551893e1171a3d6.rmeta: src/main.rs
-
-/Users/zhouzhenyong/project/rust-github/rust-learn/greeting/target/debug/deps/greeting-8551893e1171a3d6.d: src/main.rs
-
-src/main.rs:
