a6b31e4af4511a7337021d4f7408420c071fea01
shizi <lan.zhu.123@163.com>
2021-10-11 10:53:07 +0800

    add 添加head部分

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::Result;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, guard, post, web};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -21,25 +21,16 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-// #[actix_web::main]
-// async fn main() -> Result<()> {
-//     HttpServer::new(|| {
-//         App::new()
-//         .service(web::scope("/test").service(hello).service(echo))
-//     })
-//     .bind("127.0.0.1:8080")?
-//     .run()
-//     .await
-// }
-
-
 #[actix_web::main]
 async fn main() -> Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
-            .service(web::scope("/test").service(echo2))
+            .service(
+                web::scope("/test")
+                .guard(guard::Header("version", "2.0"))
+                .service(echo2))
             .route("/hey", web::get().to(manual_hello))
     })
     .bind("127.0.0.1:8080")?
