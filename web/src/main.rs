03d60369ba32045098ed15507aaa46512e0dfa94
shizi <lan.zhu.123@163.com>
2021-10-11 10:50:54 +0800

    add 添加范围的测试

--- web/src/main.rs
+++ web/src/main.rs
@@ -12,6 +12,11 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+#[post("/echo2")]
+async fn echo2(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
@@ -34,6 +39,7 @@ async fn main() -> Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(web::scope("/test").service(echo2))
             .route("/hey", web::get().to(manual_hello))
     })
     .bind("127.0.0.1:8080")?
