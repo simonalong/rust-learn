f1c0875d45a6693dd9ed7eec635b0bead471c0cf
shizi <lan.zhu.123@163.com>
2021-10-11 10:46:13 +0800

    update 恢复可用

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Result;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
@@ -10,12 +12,29 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+// #[actix_web::main]
+// async fn main() -> Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//         .service(web::scope("/test").service(hello).service(echo))
+//     })
+//     .bind("127.0.0.1:8080")?
+//     .run()
+//     .await
+// }
+
+
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<()> {
     HttpServer::new(|| {
         App::new()
-        .service(web::scope("/test").service(hello).service(echo))
-        .service(web::scope("/test").service(hello).service(echo))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
     })
     .bind("127.0.0.1:8080")?
     .run()
