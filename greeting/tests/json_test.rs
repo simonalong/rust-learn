fc1115937b78bb79d27a390b21cb0f1f6757a303
shizi <lan.zhu.123@163.com>
2021-10-18 10:59:42 +0800

    add 添加json的解析

--- greeting/tests/json_test.rs
+++ greeting/tests/json_test.rs
@@ -19,42 +19,53 @@ fn test_json1() {
     println!("Please call {} at the number {}", value["name"], value["phones"][0]);
 }
 
-#[test]
-fn test_2() {
-    let data_map: DashMap<&str, &str> = DashMap::new();
-    data_map.insert("k1", "v1");
-    data_map.insert("k2", "v2");
+// #[test]
+// fn test_2() {
+//     let data_map: DashMap<&str, &str> = DashMap::new();
+//     data_map.insert("k1", "v1");
+//     data_map.insert("k2", "v2");
+//
+//     println!("{}", serde_json::to_string(&data_map).unwrap());
+//
+//     // let mut neo_map = NeoMap::new();
+//     // neo_map.put("k1", "v1");
+//     //
+//     // println!("{}", neo_map.to_json_string());
+// }
 
-    println!("{}", serde_json::to_string(&data_map).unwrap());
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+struct NeoMap {
 
-    // let mut neo_map = NeoMap::new();
-    // neo_map.put("k1", "v1");
-    //
-    // println!("{}", neo_map.to_json_string());
+    data_map: String
 }
 
+impl NeoMap {
+    pub fn new() -> NeoMap {
+        NeoMap{data_map: String::from("test")}
+    }
+}
+
+#[test]
+fn test_json2() {
+    let data = MyData {
+        name: String::from("data"),
+        id: 321,
+    };
+    let data_json = serde_json::to_string(&data).unwrap();
+    println!("data ===== {}", data_json);
+}
+
+#[derive(Serialize, Deserialize)]
+struct MyData{
+    name: String,
+    id: i128
+}
+
+// #[test]
+// fn test_3() {
+//     let neo_map = NeoMap{data_map: String::from("haode")};
 //
-// #[derive(Deserialize, Debug, Clone, Default)]
-// struct NeoMap {
-//
-//     data_map: DashMap<String, String>
+//     println!("{}", serde_json::to_string(&neo_map).unwrap());
 // }
-//
-// impl NeoMap {
-//     pub fn new() -> NeoMap {
-//         NeoMap{data_map: DashMap::new()}
-//     }
-//
-//     pub fn put(&mut self, key:&str, value: &str) -> &mut NeoMap {
-//         let mut map = &mut self.data_map;
-//         map.insert(String::from(key), String::from(value));
-//         self
-//     }
-//
-//     pub fn to_json_string(&self) -> String {
-//         to_string(&self.data_map).unwrap()
-//     }
-// }
-//
 
 
